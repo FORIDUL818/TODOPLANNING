6067d0ea5792c0743d14dfd51e5a3ef854c49ac7
Foridul <foridulislamdise880@gmail.com>
2024-04-20 12:51:24 +0600

    error handle

--- src/Controller/userController.js
+++ src/Controller/userController.js
@@ -70,7 +70,7 @@ catch(err){
 // recover email and otp varification start
 
   exports.RecoverVaryfyEmail=async(req,res)=>{
-    let Email=req.params.email;
+    let Email=req.params.Email;
     let Otp=Math.floor(Math.random() * 1000000)
     
     try{
